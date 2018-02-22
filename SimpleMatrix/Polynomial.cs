2ba99fbf5ccca6c6a23fd32f4f6f346f63d81ec4
illbegood <s.ershov@g.nsu.ru>
2018-02-22 20:06:57 +0700

    Matrix Dominatrix
    Нужно алгоритм Евклида сделать, но там надо думать, а мне лень, может, завтра заставлю себя.

---
+++ SimpleMatrix/Polynomial.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace SimpleMatrix
+{
+    class Polynomial
+    {
+        public List<double> Coeff { get; protected set; }
+        public int Deg { get; protected set; }
+        public Polynomial(params double[] list)//check for ending zeroes
+        {
+            Coeff = new List<double>(list);
+            //here
+            Deg = Coeff.Count - 1;
+        }
+        public Polynomial(IEnumerable<double> list)//check for ending zeroes
+        {
+            Coeff = new List<double>(list);
+            //here
+            Deg = Coeff.Count - 1;
+        }
+        public static Polynomial operator +(Polynomial a, Polynomial b)//check for ending zeroes
+        {
+            return new Polynomial(a.Coeff.Zip(b.Coeff, (x, y) => x + y).Concat(a.Deg > b.Deg ? a.Coeff.Skip(b.Deg) : b.Coeff.Skip(a.Deg)));
+        }
+        //is not needed now but may be useful for GCD
+        public static Polynomial operator -(Polynomial a, Polynomial b)//check for ending zeroes
+        {
+            return new Polynomial(a.Coeff.Zip(b.Coeff, (x, y) => x - y).Concat(a.Deg > b.Deg ? a.Coeff.Skip(b.Deg) : b.Coeff.Skip(a.Deg)));
+        }
+        public static Polynomial operator *(Polynomial a, Polynomial b)
+        {
+            List<double> list = new List<double>(Math.Max(a.Deg + b.Deg + 1, 0));
+            for (int i = 0; i < a.Deg; ++i)
+                for (int j = 0; j < b.Deg; ++j)
+                    list[i + j] = a[i] * b[j];
+            return new Polynomial(list);
+        }
+        public double this[int i]
+        {
+            get { return Coeff[i]; }
+            set { Coeff[i] = value; }
+        }
+        /*
+        bool IsNotZero()
+        {
+            return Coeff.Exists(x => x != 0);
+        }
+        */
+        public static Polynomial GCD(Polynomial a, Polynomial b)//write this
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ SimpleMatrix/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SimpleMatrix
+{
+    struct Permutation
+    {
+        public List<int> seq { get; private set; }
+        public int sign { get; private set; }
+        public int size { get; }
+        public Permutation(IEnumerable<int> list)
+        {
+            seq = new List<int>(list);
+            sign = 1;
+            size = seq.Count;
+        }
+        public void Swap(int i, int j)
+        {
+            int t = seq[i];
+            seq[i] = seq[j];
+            seq[j] = t;
+            sign *= -1;
+        }
+    }
+    class Program
+    {
+        static Random r = new Random();
+        static int size;
+        static Polynomial[,] M;//square only
+        static Permutation perm;
+        static Stack<int> stack = new Stack<int>();
+        static void GenerateCharacteristicMatrix(int size)
+        {
+            Program.size = size;
+            M = new Polynomial[size, size];
+            for (int i = 0; i < size; ++i)
+                for (int j = 0; i < size; ++j)
+                {
+                    double t = r.NextDouble();
+                    M[i, j] = i == j ? new Polynomial(t, -1) : new Polynomial(t);
+                }   
+        }
+        static IEnumerable<Permutation> Permutations(int depth = 0)
+        {
+            if (depth == perm.size - 1)
+                yield return perm;
+            else
+                for (int i = depth + 1; i < perm.size; ++i)
+                {
+                    foreach (Permutation x in Permutations(depth + 1))
+                        yield return x;
+                    perm.Swap(depth, i);
+                }
+        }
+        static Polynomial Minor(int i, int j)
+        {
+            int[] li = (int[])Enumerable.Range(0, i).Concat(Enumerable.Range(i + 1, size - i - 1));
+            perm = new Permutation(Enumerable.Range(0, j).Concat(Enumerable.Range(j + 1, size - j - 1)));
+            Polynomial ret = new Polynomial(0);
+            foreach (Permutation p in Permutations())
+            {
+                Polynomial mul = new Polynomial(p.sign);
+                for (int k = 0; k < size; ++k)
+                    mul *= M[li[k], p.seq[k]];
+                ret += mul;
+            }
+            return ret;
+        }
+        static bool ToChtoNado()
+        {
+            int j = 1;
+            Polynomial gcd = Minor(0, 0);
+            for (int i = 0; i < size; ++i)
+            {
+                for (; j < size; ++j)
+                {
+                    gcd = Polynomial.GCD(gcd, Minor(i, j));
+                    if (gcd.Deg <= 0)
+                        return false;
+                }
+                j = 0;
+            }
+            return true; 
+        }
+        static void Main(string[] args)
+        {
+            GenerateCharacteristicMatrix(5);
+            Console.WriteLine(ToChtoNado());
+        }
+    }
+}
